a428f98ba7343d986b7732e6a6a71d826152406a
Tim Branyen <tim@tabdeveloper.com>
2012-11-27 02:36:48 -0500

    more cleanup

--- lib/cli.lua
+++ lib/cli.lua
@@ -1,23 +1,43 @@
--- Include modules.
+-- Include modules. -----------------------------------------------------------
+
 local cli = require "cliargs"
 local colors = require "ansicolors"
 local tlua = require "tlua"
 
--- Set the name to show up.
+-- Set the name to show up. ---------------------------------------------------
+
 cli:set_name("webapp")
 
--- Display the version.
+-- Display the version. -------------------------------------------------------
+
 cli:add_flag("-v, --version", "Displays webapp's version and exits.")
 
--- Parses from _G['arg']
-local args = cli:parse_args()
+-- Override the error function. -----------------------------------------------
 
-if not args then
-  -- something wrong happened and an error was printed
-  return
+function cli_error(msg, noprint, cli)
+  local name = colors("%{dim white}" .. (cli.name or "") .. "%{reset}")
+  local err = colors("%{red}Command not recognized.%{reset}")
+
+  local msg = name .. ": " .. err .. " Run with --help for usage."
+
+  if not noprint then
+    print("\n" .. msg .. "\n")
+  end
+
+  return nil, msg
 end
 
--- Checking for flags: is -v or --version set?
+-- Parses from _G['arg']. -----------------------------------------------------
+
+local args = cli:parse_args()
+
+if not args or #args == 0 then
+  return cli_error("", false, cli)
+end
+
+-- Checking for flags: is -v or --version set? --------------------------------
+
 if args["v"] then
-  return print(colors("%{dim white}webapp%{reset} - WebApp Builder - 1.0.0\n"))
+  local msg = colors("%{dim white}webapp%{reset} - WebApp Builder - 1.0.0")
+  return print("\n" .. msg .. "\n")
 end
