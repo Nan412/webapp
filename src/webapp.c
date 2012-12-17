873560056a2b81ed4133d3641b518857525804c9
Tim Branyen <tim@tabdeveloper.com>
2012-12-16 21:33:05 -0500

    works globally now, able to locate original location

--- src/webapp.c
+++ src/webapp.c
@@ -1,14 +1,17 @@
+// Standard libraries.
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include <unistd.h>
+#include <unistd.h>
+#include <libgen.h>
 
-// Include the Lua headers.
+// Include Lua.
 #include "../deps/lua/src/lua.h"
 #include "../deps/lua/src/luaconf.h"
 #include "../deps/lua/src/lauxlib.h"
 #include "../deps/lua/src/lualib.h"
 
+// Windows support.
 #if _WIN32 || _WIN64
 #include <Windows.h>
 #define snprintf _snprintf
@@ -19,89 +22,109 @@
 //
 // http://stackoverflow.com/questions/606041/how-do-i-get-the-path-of-a-process
 // -in-unix-linux
-//char* find_execution_path(char* path, size_t dest_len, char* argv0) {
-//  char* baseName = NULL;
-//  char* systemPath = NULL;
-//  char* candidateDir = NULL;
-//
-//  // The easiest case: Linux.  If it's not here, there is no guarentee.
-//  if (readlink("/proc/self/exe", path, dest_len) != -1) {
-//    dirname(path);
-//    strcat(path, "/");
-//
-//    return path;
-//  }
-//
-//  /* check if we have something like execve("foobar", NULL, NULL) */
-//  if (argv0 == NULL) {
-//    /* we surrender and give current path instead */
-//    if (getcwd (path, dest_len) == NULL) return NULL;
-//    strcat  (path, "/");
-//
-//    return path;
-//  }
-//
-//  /* argv[0] */
-//  /* if dest_len < PATH_MAX may cause buffer overflow */
-//  if ((realpath (argv0, path)) && (!access (path, F_OK))) {
-//    dirname (path);
-//    strcat  (path, "/");
-//
-//    return path;
-//  }
-//
-//  /* Current path */
-//  baseName = basename (argv0);
-//  if (getcwd (path, dest_len - strlen (baseName) - 1) == NULL) {
-//    return NULL;
-//  }
-//
-//  strcat(path, "/");
-//  strcat(path, baseName);
-//
-//  if (access (path, F_OK) == 0) {
-//    dirname (path);
-//    strcat  (path, "/");
-//    return path;
-//  }
-//
-//  /* Try the PATH. */
-//  systemPath = getenv ("PATH");
-//  if (systemPath != NULL) {
-//    dest_len--;
-//    systemPath = strdup (systemPath);
-//
-//    for (candidateDir = strtok (systemPath, ":"); candidateDir != NULL; candidateDir = strtok (NULL, ":")) {
-//      strncpy (path, candidateDir, dest_len);
-//      strncat (path, "/", dest_len);
-//      strncat (path, baseName, dest_len);
-//
-//      if (access(path, F_OK) == 0) {
-//        free (systemPath);
-//        dirname (path);
-//        strcat  (path, "/");
-//
-//        return path;
-//      }
-//    }
-//
-//    free(systemPath);
-//    dest_len++;
-//  }
-//
-//  /* again someone has use execve: we dont knowe the executable name; we surrender and give instead current path */
-//  if (getcwd (path, dest_len - 1) == NULL) {
-//    return NULL;
-//  }
-//
-//  strcat(path, "/");
-//
-//  return path;
-//}
+char* find_execution_path(char* path, size_t dest_len, char* argv0) {
+  char* baseName = NULL;
+  char* systemPath = NULL;
+  char* candidateDir = NULL;
+  char buf[FILENAME_MAX];
+  size_t len;
+
+  // The easiest case: Linux.  If it's not here, there is no guarentee.
+  if (readlink("/proc/self/exe", path, dest_len) != -1) {
+    dirname(path);
+    strcat(path, "/");
+
+    return path;
+  }
+
+  /* check if we have something like execve("foobar", NULL, NULL) */
+  if (argv0 == NULL) {
+    /* we surrender and give current path instead */
+    if (getcwd(path, dest_len) == NULL) {
+      return NULL;
+    }
+
+    strcat(path, "/");
+
+    return path;
+  }
+
+  /* argv[0] */
+  /* if dest_len < PATH_MAX may cause buffer overflow */
+  if ((realpath (argv0, path)) && (!access (path, F_OK))) {
+    dirname(path);
+    strcat(path, "/");
+
+    return path;
+  }
+
+  /* Current path */
+  baseName = basename(argv0);
+
+  if (getcwd (path, dest_len - strlen (baseName) - 1) == NULL) {
+    return NULL;
+  }
+
+  strcat(path, "/");
+  strcat(path, baseName);
+
+  if (access(path, F_OK) == 0) {
+    dirname(path);
+    strcat(path, "/");
+
+    return path;
+  }
+
+  /* Try the PATH. */
+  systemPath = getenv ("PATH");
+
+  if (systemPath != NULL) {
+    dest_len--;
+    systemPath = strdup(systemPath);
+
+    for (candidateDir = strtok(systemPath, ":"); candidateDir != NULL; candidateDir = strtok(NULL, ":")) {
+      strncpy(path, candidateDir, dest_len);
+      strncat(path, "/", dest_len);
+      strncat(path, baseName, dest_len);
+
+      if (access(path, F_OK) == 0) {
+        free(systemPath);
+        dirname(path);
+        strcat(path, "/");
+
+        // Strip off the trailing `/`.
+        path[strlen(path)-1] = '\0';
+
+        // Attempt checking if this is a symbolic link.
+        if ((len = readlink(path, path, sizeof(buf)-1)) != -1) {
+          path[strlen(path)-10] = '\0';
+
+          // Symbolic link path.
+          return path;
+        }
+
+        // Hard path.
+        return path;
+      }
+    }
+
+    free(systemPath);
+    dest_len++;
+  }
+
+  /* again someone has use execve: we dont knowe the executable name; we surrender and give instead current path */
+  if (getcwd (path, dest_len - 1) == NULL) {
+    return NULL;
+  }
+
+  strcat(path, "/");
+
+  return path;
+}
 
 // ~ Read Lua configuration file. ~
-int load_lua_cli(char** buffer) {
-  FILE* file = fopen("lib/cli.lua", "rb");
+int load_lua_cli(char* root, char** buffer) {
+  FILE* file = fopen(strncat(root, "lib/cli.lua", FILENAME_MAX), "rb");
   int length;
 
   // FIXME Visual Studio complains that this is safer, is it?
@@ -147,11 +170,9 @@ int load_lua_cli(char** buffer) {
 
 // Kick off the tool!
 int main (int argc, char** argv) {
-  char* currentPath = ".";
+  //char* currentPath = ".";
   // FIXME Weird segmentation fault when running the following code.
-  //char currentPath[FILENAME_MAX];
-  // Set the current path.
-  //find_execution_path(currentPath, sizeof(currentPath), argv[0]);
+  char currentPath[FILENAME_MAX];
   // Used to store the contents of the CLI script.
   char* buff;
   // Used for iteration and tracking errors.
@@ -160,6 +181,9 @@ int main (int argc, char** argv) {
   char *paths, *absolutePaths, *cpaths, *absoluteCPaths;
   // Maintain the Lua runtime state.
   lua_State* L;
+
+  // Set the current path.
+  find_execution_path(currentPath, sizeof(currentPath), argv[0]);
 
   // The template string for Lua module paths.
   paths =
@@ -216,11 +240,12 @@ int main (int argc, char** argv) {
   lua_pop(L, 1);
   
   // Open file buffer.
-  error = load_lua_cli(&buff);
+  error = load_lua_cli(currentPath, &buff);
 
   // File read error.
   if (error < 0) {
     fprintf(stderr, "%s", "Unable read or parse the main CLI file.");
+    return 1;
   }
 
   // Load the buffer.
